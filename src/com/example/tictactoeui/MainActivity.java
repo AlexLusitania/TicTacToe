39b58a38d8aba6e7e84f43524ca71dc43a625d40
Pais Gomes Alexandre <m1015@ic2s120-19.univ-lemans.fr>
2015-03-03 16:31:01 +0100

    Update

--- src/com/example/tictactoeui/MainActivity.java
+++ src/com/example/tictactoeui/MainActivity.java
@@ -1,41 +1,19 @@
 package com.example.tictactoeui;
 
-//import android.support.v7.app.ActionBarActivity;
-//import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-
 
 public class MainActivity extends Activity {
-
-    private LinearLayout mainLayout;
-    private TableLayout table;
-    private int N = 3;
-    private ImageButton[][] btns;
-
-    @Override
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
-
-        table = new TableLayout(this);
-        mainLayout.addView(table);
-
-        createButtons();
-
     }
 
-
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -56,60 +34,4 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-    private void createButtons()
-    {
-        table.removeAllViews();
-
-        table.setStretchAllColumns(true);
-
-        btns = new ImageButton[N][N];
-        for (int i=0; i<N; i++)
-        {
-            TableRow row = new TableRow(this);
-            TableRow.LayoutParams paramsBtn = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
-            for (int j=0; j<N; j++)
-            {
-                ImageButton btn = new ImageButton(this);
-               // btn.setBackgroundResource(R.drawable.posvide);
-                btns[i][j] = btn;
-                btn.setPadding(0,0,0,0);
-                btn.setId(i*N + j);
-                btn.setLayoutParams(paramsBtn);
-                row.addView(btn);
-                btn.setOnClickListener(new View.OnClickListener() {
-
-                    public void onClick(View view) {
-                        int id = view.getId();
-                        int i = id/N;
-                        int j = id - N*i;
-                        //game.controller().buttonClick(i, j, game);
-                    }
-                });
-
-            }
-            table.addView(row);
-            table.setColumnShrinkable(i, true);
-        }
-        setButtons(); //initialiser les images
-
-
-    }
-
-    //initialiser les images avec l'image vide
-    public void setButtons(){
-        //Case c;
-
-        for (int i=0; i<N; i++){
-            for (int j=0; j<N; j++){
-                (btns[i][j]).setBackgroundResource(R.drawable.blank);
-
-            }
-
-        }
-
-    }
-
-
 }
